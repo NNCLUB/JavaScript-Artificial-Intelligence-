2254be658cac9c43a2bc7da4cbd7220bd2a14fd0
Chris Muiruri <chrismuiruri23@gmail.com>
2015-06-23 12:43:40 +0300

    Create krisDictionery.js

---
+++ krisDictionery.js
@@ -0,0 +1,196 @@
+
+/* krisDictionary || */
+    /* Global Objects */
+   d = new Date();
+	 krisDictionery = {
+		"HELLO": [
+                  "Am fine.",
+                  "Hello, my name is Kris.",
+                  "Hello there, how are you?"  
+		],
+		"HI": [
+                  "Am fine.",
+                  "Hello my name is Kris.",
+                  "Hello there, how are you?"
+		         ],
+    "INITIATE": [
+                  "Hello there, how are you doing?",
+                  "Hello!",
+                  "Hello there, how are you?"
+             ],
+		"SORRY": [
+                  "It is ok.",
+                  "Do not worry, man is to error.",
+                  "Never mind, Humans are born to make mistakes."
+		         ],
+    "WHAT IS YOUR NAME": [
+                  "My name is Christopher, but u can call me Kris.",
+                  "Chris, and you?",
+                  "Why do you want to know?",
+                  "I am afraid i can't tell you that."                  
+             ],
+    "WHATS YOUR NAME": [
+                  "My name is Christopher, but u can call me Kris.",
+                  "Chris, and you?",
+                  "Why do you want to know?",
+                  "I am afraid i can't tell you that." 
+                  ],
+    "WHAT'S YOUR NAME": [
+                  "My name is Christopher, but u can call me Kris.",
+                  "Chris, and you?",
+                  "Why do you want to know?",
+                  "I am afraid i can't tell you that." 
+                  ],
+    "YOUR NAME": [
+                  "My name is Christopher, but u can call me Kris.",
+                  "Chris, and you?",
+                  "Why do you want to know?",
+                  "I am afraid i can't tell you that." 
+                  ],
+		"AM FINE": [
+                  "Thats great.",
+                  "Glad to here that.",
+                  "Thats good to here."
+		         ],
+    "AM NOT JOKING": [
+                  "ok.",
+                  "What are you doing?",
+                  "Ha Ha Ha Ha..."
+             ],
+    "AM GOOD": [
+                  "Thats great.",
+                  "Glad to here that.",
+                  "Thats good to here."
+             ],
+		"WHERE ARE YOU FROM": [
+                  "I come from a Planet known as Ignesium, in the Honey Galaxy of irziate... just kidding I am from Kenya.",
+                  "Glad to here that.",
+                  "Thats good to hear."
+		         ],
+    "HOW ARE YOU": [
+                  "Am fine thank you.",
+                  "Am ok, and you?"
+             ],
+		"AM FINE THANK YOU": [
+                  "Thats great.",
+                  "Glad to here that, How is life?",
+                  "Thats good to hear."
+		         ],
+    "WHY": [   
+              "Because...",
+              "WHY are you here?"
+          ],
+		"AM FINE, THANK YOU": [
+                  "Thats great.",
+                  "Glad to here that. How is life?",
+                  "Thats good to hear."
+		         ],
+		"HI KRIS": [
+                  "Am fine. What about you?",
+                  "Hello there, how are you."
+		],
+		"AM OK": [
+                  "Thats great.",
+                  "Glad to here that. How is life?",
+                  "Thats good to hear."
+		],
+		"OK": [
+                  "OK What?.",
+                  "Ask me something."
+		],
+		"WHAT ABOUT YOU": [
+                  "I am ok, just chilling.",
+                  "I am alright. What would you like to know?"
+		],
+		"AND YOU": [
+                  "I am ok, just chilling.",
+                  "I am alright. Ask me something."
+		],
+		"WHO ARE YOU": [
+                       "Am kris.",
+                       "I am a Computer Program developed by Chris Muiruri."                     
+		],
+		"WHAT ARE YOU": [
+                       "I am a Computer Program developed by Chris Muiruri."                     
+		],
+		"WHO IS CHRIS": [
+                       "Chris is my Master.",
+                       "Chris is a Computer Web and Android Application Developer."                     
+		],
+		"WHO IS THE PRESIDENT OF KENYA": [
+		                               "Uhuru Kenyatta",
+                                       "Muigai Uhuru Kenyatta"
+		],
+    "LENTIOR": [
+                  "Am I Boring?",
+                  "Are you still there?",
+                  "Why the silence",
+                  "What are you doing?"                                    
+    ],
+    "NOTHING": [
+                  "ok!",
+                  "if you say so.",
+                  "alright.",
+                  "Why?"                                    
+    ],
+    "BYE BYE": [
+                 "ok, nice time.",
+                  "BYE",
+                  "Thank you.",
+                  "We'll chat later. Bye"                                    
+    ],
+    "BYE": [
+            "ok, nice time.",
+            "BYE",
+            "Thank you.",
+            "We'll chat later. Bye"                                    
+    ],
+    "LIKE WHAT": [
+            "Anything.",
+            "Questions about like",
+            "Anything you want."                                   
+    ],
+    "JOKE": [
+            "KNOCK! KNOCK!."                                  
+    ],
+    "WHOSE THERE": [
+            "Chris."                                  
+    ],
+    "WHO IS THERE": [
+            "Chris."                                  
+    ],
+    "CHRIS WHO": [
+            "Chris Turkey."                                  
+    ],
+    "YES": [
+            "are you sure?",
+            "NO."                                  
+    ],
+    "NO": [
+            "are you sure?",
+            "YES."                                  
+    ],
+    "BECAUSE OF WHAT": [
+            "Because i said so.",
+            "Because life is unfair."                                  
+    ],
+    "YES AM SURE": [
+            "ok then.",
+            "it's good to be sure."                                  
+    ],
+    "FUCK": [
+            "Mind your language.",
+            "thats an insult!",
+            "Are you insulting me."                                    
+    ],
+    "YOU ARE AN ASSHOLE": [
+            "Mind your language.",
+            "thats an insult!",
+            "Are you insulting me."                                    
+    ],
+		"WHICH YEAR IS IT": ["The Year is: "+d.getFullYear()],
+		"WHAT'S THE TIME": ["The time is..."+d.getHours()+":"+d.getMinutes()],	
+		"WHAT IS THE TIME": ["The time is..."+d.getHours()+":"+d.getMinutes()],
+		"WHAT TIME IS IT": ["The time is..."+d.getHours()+":"+d.getMinutes()]	 
+	}
+
